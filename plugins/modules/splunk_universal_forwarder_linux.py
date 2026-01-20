cf9201089e7b9de0a8825c75db992bdd81cfdd36
Shahar Golshani <sgolshan@redhat.com>
2026-01-20 17:42:34 +0200

    test pre-commit integration tests trigger

--- plugins/modules/splunk_universal_forwarder_linux.py
+++ plugins/modules/splunk_universal_forwarder_linux.py
@@ -485,13 +485,7 @@ def remove_deployment_server(module: AnsibleModule, splunk_home: str) -> bool:
     """Remove the deployment server by deleting deploymentclient.conf and restarting Splunk."""
     if module.check_mode:
         return True
-    deployment_conf = os.path.join(
-        splunk_home,
-        "etc",
-        "system",
-        "local",
-        "deploymentclient.conf",
-    )
+    deployment_conf = os.path.join(splunk_home, "etc", "system", "local", "deploymentclient.conf")
     if os.path.exists(deployment_conf):
         try:
             os.remove(deployment_conf)
